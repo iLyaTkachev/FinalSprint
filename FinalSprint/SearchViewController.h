ac95603ca7bd8d9b9674d59dc6d3e31d0a24ec2c
iLya Tkachev <ilya@iLyas-Mac.local>
2017-05-26 16:39:12 +0300

    problems with genres

---
+++ FinalSprint/SearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.h
+//  FinalSprint
+//
+//  Created by iLya Tkachev on 5/26/17.
+//  Copyright © 2017 iLya Tkachev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchViewController : UIViewController
+
+@end
